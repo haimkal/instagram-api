664e6a400b1d9730898fbf44b552579d869c7153
Haim Kalvo <you@example.com>
2021-02-08 16:57:50 +0200

    adding css to register form

--- instagram-ui/src/App.js
+++ instagram-ui/src/App.js
@@ -1,10 +1,15 @@
-
-import './App.css';
+import Header from './Header/Header';
+import Register from './Register/Register';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="container">
+        <Register />
+      </div>
+          
     </div>
   );
 }
